a8fee9a080c8675dfa34c0b85bfc2ee1fcaef58e
Steve <steveruizok@gmail.com>
2019-07-27 11:20:19 +0100

    Removes spinners.

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
+#! /usr/bin/env node
+
 const fs = require('fs')
 const clone = require('git-clone')
 const chalk = require('chalk')
 const { execSync } = require('child_process')
-const ora = require('ora')
 
-let [, main, dir = 'docs'] = process.argv
+let [, , dir = 'docs'] = process.argv
 
-// Log a message using chalk
+// Log messages using chalk
 const clear = console.clear
 const log = console.log
 const err = (m) => log(chalk.yellow(m))
 const term = (m) => chalk.bold.yellow(m)
 const bold = (m) => chalk.bold(m)
 
+// Start installation
 function handleStart() {
 	clear()
 	log(bold('Creating docs starter'))
@@ -24,55 +26,39 @@ function handleComplete() {
 	installDependencies()
 
 	clear()
-	log(`Docs ready!`)
+	log(`Docs ready at bold(/${dir})!`)
 	log(``)
 	log(bold(`Editing:`))
 	log(`✦ Create your site's content in the ${bold(`content`)} folder.`)
-	log(`✦ Edit your site's navigation in the ${bold(`nav`)} folder.`)
+	log(`✦ Edit your site's navigation in the ${bold(`content/nav`)} folder.`)
 	log(`✦ Edit your site's theme in the ${bold(`src`)} folder.`)
 	log(``)
 	log(bold(`Running:`))
 	log(`✦ Type ${term(`cd ${dir}`)} and press enter.`)
-	log(`✦ Type ${term(`npm run start`)} to start the local server.`)
+	log(`✦ Type ${term(`npm start`)} to start the local server.`)
 	log(``)
 	log(`Happy writing!`)
 	log(``)
 }
 
+// Install dependencies in project folder
 function installDependencies() {
-	let spinner = ora('Installing dependencies...').start()
+	log('Installing dependencies – this may take a minute')
 	execSync(`cd ${dir} && npm i`)
-	spinner.stop()
 }
 
-function handleError() {
-	err(`Oops, that directory (${dir}) already exists.`)
-}
-
+// Clone repository into project folder
 async function cloneRepo() {
-	let spinner = ora(`Cloning repo into /${dir}...`).start()
+	log(`Cloning repo into ${dir}`)
 	if (fs.existsSync(dir)) {
-		spinner.stop()
-		handleError()
+		err(`Oops, that directory (${dir}) already exists.`)
 		return
 	}
 
-	// fs.mkdir(`${dir}`, {}, () => {
-	// 	console.log('entering folder')
-	// 	execSync(`cd ${dir} && npm i quick-docs`)
-	// })
-
-	// execSync(`cd ${dir}`)
-	// console.log('installing docs')
-	// execSync(`npm i quick-docs`)
-	// handleComplete()
-
 	clone('https://github.com/steveruizok/quick-docs', `./${dir}`, {}, () => {
-		spinner.stop()
 		handleComplete()
 	})
 }
 
 // Kickoff
-
 handleStart()
